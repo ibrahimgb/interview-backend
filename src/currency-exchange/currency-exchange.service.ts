037757ffb394fef17bb5623b794f7c89c7a08a26
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-06-25 14:52:12 +0100

    read currency from database

--- src/currency-exchange/currency-exchange.service.ts
+++ src/currency-exchange/currency-exchange.service.ts
@@ -1,73 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { PrismaService } from '../prisma/prisma.service';
 const axios = require('axios');
 const fs = require('fs');
 const { WebClient } = require('@slack/web-api');
 @Injectable()
 export class CurrencyExchangeService {
-  //   storage = diskStorage({
-  //     destination: (req, file, cb) => {
-  //       // Specify the directory where the file will be saved
-  //       const directory = './uploads';
-  //       cb(null, directory);
-  //     },
-  //     filename: (req, file, cb) => {
-  //       // Set a custom file name
-  //       cb(null, 'data.json');
-  //     },
-  //  });
-  constructor() {
-    // const readJsonFile = (req, res) => {
-    //   try {
-    //     const filePath = './data/data.json';
-    //     const jsonData = fs.readFileSync(filePath, 'utf-8');
-    //     const parsedData = JSON.parse(jsonData);
-    //     res.json(parsedData);
-    //   } catch (error) {
-    //     console.error('Error reading JSON file:', error);
-    //     res.status(500).json({ error: 'Failed to read JSON file.' });
-    //   }
-    // };
-    // console.log(readJsonFile);
-  }
+  constructor(private database: PrismaService) {}
 
-  token = 'SLACK_API_TOKEN';
-  web = new WebClient(this.token);
-
-  currencyList = ['USD', 'GBP'];
-  currencyMinRef = { USD: 0.05, GBP: 0.05 };
-  currencyMaxRef = { USD: 0.1, GBP: 0.1 };
-  currencyStoreRef = { USD: 1, GBP: 0.88 };
-  currentExchangeRate = { USD: 1.09, GBP: 0.853 };
+  currentExchangeRate;
+  myExchangeRate;
 
   async getMyExchangeRate() {
-    const transformedData = this.currencyList.map((currency) => {
+    let res = await this.database.currency.findMany();
+
+    const transformedData = res.map((currency) => {
       return {
-        currency: currency,
-        storeExchangeRate: this.currencyStoreRef[currency],
-        minExchangeRate: this.currencyMinRef[currency],
-        maxExchangeRate: this.currencyMaxRef[currency],
-        currentExchangeRate: this.currentExchangeRate[currency],
+        ...res[0],
+        currentExchangeRate: this.currentExchangeRate[currency.currency],
       };
     });
+
+    this.myExchangeRate = transformedData;
 
     return transformedData;
   }
 
   async getExchangeRate(): Promise<Object> {
+    const filter_list = await this.database.currency.findMany({
+      select: {
+        currency: true,
+        storeExchangeRate: false,
+        minExchangeRate: false,
+        maxExchangeRate: false,
+      },
+    });
+
+    console.log(filter_list);
+
+    function getReducedList(res, filter_list) {
+      return Object.keys(res)
+        .filter((key) => filter_list.some((obj) => obj.currency === key))
+        .reduce((acc, key) => {
+          acc[key] = res[key];
+          return acc;
+        }, {});
+    }
+
     try {
       const response = await axios.get(
         'https://api.exchangerate-api.com/v4/latest/EUR',
       );
       const rates = response.data.rates;
-      const currencyExchange = {
-        USD: 1,
-        ...this.currencyList.reduce((exchange, currency) => {
-          exchange[currency] = rates[currency];
-          return exchange;
-        }, {}),
-      };
+      const currencyExchange = getReducedList(rates, filter_list);
+
       //console.log(currencyExchange);
+      this.currentExchangeRate = currencyExchange;
       return currencyExchange;
     } catch (error) {
       console.error('Error fetching exchange rate:', error.message);
@@ -75,29 +63,15 @@ export class CurrencyExchangeService {
     }
   }
 
+  getByCurrency(list, key) {
+    list.filter((item) => {
+      item.currency = key;
+    });
+  }
+
   checkIfProblem() {
     this.getExchangeRate().then((res) => {
       console.log(res);
-      for (const currency in res) {
-        if (res.hasOwnProperty(currency)) {
-          const value = res[currency];
-          const refValue = this.currencyMinRef[currency];
-
-          if (value < refValue - this.currencyStoreRef[currency]) {
-            this.triggerErrer(currency, value, refValue, false);
-          }
-        }
-      }
-      for (const currency in res) {
-        if (res.hasOwnProperty(currency)) {
-          const value = res[currency];
-          const refValue = this.currencyMaxRef[currency];
-
-          if (value > refValue + this.currencyStoreRef[currency]) {
-            this.triggerErrer(currency, value, refValue, true);
-          }
-        }
-      }
     });
   }
 
@@ -128,9 +102,16 @@ export class CurrencyExchangeService {
   }
 
   editCurrency(obj) {
-    this.currencyMinRef[obj.currency] = obj.currencyMinRef;
-    this.currencyMaxRef[obj.currency] = obj.currencyMaxRef;
-    this.currencyStoreRef[obj.currency] = obj.currencyStoreRef;
+    this.database.currency.update({
+      where: {
+        currency: obj.currency,
+      },
+      data: {
+        minExchangeRate: obj.currencyMinRef,
+        maxExchangeRate: obj.currencyMaxRef,
+        storeExchangeRate: obj.currencyStoreRef,
+      },
+    });
   }
 
   @Cron('* * * * * *')
