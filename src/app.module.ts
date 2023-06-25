09df92249b41c783178009d2b9a0697d878636dc
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-06-25 14:54:09 +0100

    update & cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CurrencyExchangeModule } from './currency-exchange/currency-exchange.module';
 import { EmailService } from './email/email.service';
-
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { SlackBootService } from './slack-boot/slack-boot.service';
 @Module({
-  imports: [ScheduleModule.forRoot(), CurrencyExchangeModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+    CurrencyExchangeModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, EmailService],
+  providers: [AppService, EmailService, PrismaService, SlackBootService],
 })
 export class AppModule {}
